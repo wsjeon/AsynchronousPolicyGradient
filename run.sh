cc71203c60e0060ae4e643d8d058d5aafe3cac01
wsjeon <wonsjeon@kaist.ac.kr>
2017-05-17 22:17:57 +0900

    Modify run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-# Asynchronous learning using shell script.
+# Asynchronous reinforcement learning
 #
-# Kill 'ps' job after training. 
+# Kill 'ps' job after training.
 #
 # Wonseok Jeon at KAIST
 # wonsjeon@kaist.ac.kr
+
+NUM_WORKERS=1
+
 python main.py --job_name 'ps' --task_index 0 &
-PID=$!
-python main.py --job_name 'worker' --task_index 0
-kill $PID
+PID[0]=$!
+
+task_index=0
+while [ "$task_index" -lt "$NUM_WORKERS" ]; do
+	python main.py --job_name 'worker' --task_index $task_index &
+	PID[$(($task_index + 1))]=$!
+	task_index=$(($task_index + 1))
+done
+
+task_index=0
+while [ "$task_index" -lt "$NUM_WORKERS" ]; do
+	task_index=$(($task_index + 1))
+	wait ${PID[$task_index]}
+done
+
+kill ${PID[0]}
